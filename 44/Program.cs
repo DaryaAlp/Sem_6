486699dabe5855bb4f6b97cf6c4697253d06c55d
Darya <d.a_alpatskaya@mail.ru>
2023-04-10 14:52:37 +0300

    Добавили задачи 39, 40, 42, 44, 45

---
+++ 44/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//  Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5-> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// <aside>
+// ❗ Числа Фибоначчи — числовая ****последовательность,
+//  каждый элемент которой равен сумме двух предыдущих.
+
+Console.Clear();
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"[{String.Join(' ', Fib(n))}]");
+
+int[] Fib(int n){
+    int[] mas_f = new int[n];
+    mas_f[0] = 0;
+    if(n >= 2){
+        mas_f[1] = 1;
+    }else { return mas_f; }
+    for (int i = 2; i < n; i++){
+        mas_f[i] = mas_f[i - 1] + mas_f[i - 2];
+    }
+    return mas_f;
+}
+/* Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine()!);
+        int first = 0;
+        int second = 1;
+        int temp = 0;
+        Console.WriteLine(first);
+        for (int i = 0; i < num-1; i++)
+        {
+            Console.WriteLine(second);
+            temp = second;
+            second = second + first;
+            first = temp;
+        }
+        //0, 1, 1, 2, 3, 5, 8, 13, 21*/
---
+++ 45/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Задача 45: Напишите программу,
+ которая будет создавать копию заданного массива с помощью поэлементного копирования.*/
+
+ Console.Clear();
+
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine(String.Join(" ", array));
+
+int[] copyArray = CopyArray(array);
+Console.WriteLine(String.Join(" ", CopyArray(array)));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] CopyArray(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[i];
+
+    }
+    return result;
+}
+
+/*int[] ArrayAll = GenerateArray(); // задаем инфу для метода создания массива
+        Console.WriteLine($"[{String.Join(", ", ArrayAll)}]");
+        int[] newArray2 = copyMe(ArrayAll, ArrayAll.Length);
+        Console.WriteLine($"[{String.Join(", ", newArray2)}]");
+
+        int[] GenerateArray() // генератор массива с произвольными числами
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = new Random().Next(-100, 100);
+            }
+            return array;
+        }
+
+        int[] copyMe(int[] array, int len)
+        {
+            int[] newArray = new int[len];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            return newArray;
+        }*/
